e66acabd1d886776455f41ec125fc1182c04dc08
Dyan Kastutara <dyankastutara19@gmail.com>
2025-08-06 14:59:23 +0700

    fix backup

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const app = express();
 // Schedule cron job tiap jam 2 pagi
 cron.schedule("0 2 * * *", () => {
   console.log("Starting database backup...");
-  // backupdb.postgresMainDB();
-  backupdb.mongodbMainDB();
+  backupdb.postgresMainDB();
 });
 
+// Schedule cron job tiap jam 2 pagi
+cron.schedule("0 2 * * 1", () => {
+  console.log("Senin Starting database backup...");
+  backupdb.mongodbMainDB();
+  backupdb.mongodbCRMDB();
+  backupdb.mongodbLandingPageDB();
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server backup DB running on port ${PORT}`));
