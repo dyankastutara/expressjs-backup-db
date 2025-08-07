847fa323670525f2cf3341ec5138b6ee1947a3b7
Dyan Kastutara <dyankastutara19@gmail.com>
2025-08-07 08:53:32 +0700

    add timezone on cron

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ const backupdb = require("./services/backupdb.service");
 const app = express();
 
 // Schedule cron job tiap jam 2 pagi
-cron.schedule("0 2 * * *", () => {
-  console.log("Starting database backup PG at 2am ...");
-  backupdb.postgresMainDB();
-});
+cron.schedule(
+  "0 2 * * *",
+  () => {
+    console.log("Starting database backup PG at 2am ...");
+    backupdb.postgresMainDB();
+  },
+  {
+    timezone: "Asia/Jakarta",
+  }
+);
 
 // Schedule cron job tiap jam 2 pagi
-cron.schedule("0 2 * * 1", () => {
-  console.log("Starting database backup day (Monday)...");
-  backupdb.mongodbMainDB();
-  backupdb.mongodbCRMDB();
-  // backupdb.mongodbLandingPageDB();
-});
+cron.schedule(
+  "0 2 * * 1",
+  () => {
+    console.log("Starting database backup day (Monday)...");
+    backupdb.mongodbMainDB();
+    backupdb.mongodbCRMDB();
+    // backupdb.mongodbLandingPageDB();
+  },
+  {
+    timezone: "Asia/Jakarta",
+  }
+);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server backup DB running on port ${PORT}`));
